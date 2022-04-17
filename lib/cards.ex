6799e3dd2fdf9276e5566bee49e3629f1c4e881c
SamTredgett <sam.tredgett@gmail.com>
2022-04-18 00:18:48 +0100

    Added basic functions for creating a deck, dealing a hand of a custom size and checking if a deck contains a card

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,11 +1,24 @@
 defmodule Cards do
   # Create a list of playing cards
   def create_deck do 
-    ["Ace", "Two", "Three"]
+    values =  ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Jack", "Queen", "King"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    # can add multiple loops here
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
   end
 
   def shuffle(deck) do
    Enum.shuffle(deck) 
   end
 
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do 
+    Enum.split(deck, hand_size)
+  end
 end
